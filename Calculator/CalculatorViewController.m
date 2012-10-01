6731ad47423b49fd13ffc49d9528b661ebad5baa
allenlinli <allenlinli@gmail.com>
2012-10-01 17:55:40 +0800

    temp

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -10,10 +10,11 @@
 #import "CalculatorBrain.h"
 
 @interface CalculatorViewController()
-@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
 @property (nonatomic, strong) CalculatorBrain *brain;
-@property (nonatomic) BOOL IsAfterFloatPoint;
+//@property (nonatomic) BOOL IsAfterFloatPoint;
+//@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
 @property NSInteger display2LastPressed; //0 be initial, 1 be digit, 20 be dot, 21 be digit after dot 
+@property NSInteger displayLastPressed;  //0 be initial, 1 be digit, 20 be dot, 21 be digit after dot 
 @end
 
 @implementation CalculatorViewController
@@ -41,56 +42,72 @@
         }
         else{
             self.display.text = digit;
-            self.userIsInTheMiddleOfEnteringANumber = YES;
+            self.displayLastPressed = 1;
         }
     }
-    else if(self.userIsInTheMiddleOfEnteringANumber){
+    else if(self.displayLastPressed>0){  //self.userIsInTheMiddleOfEnteringANumber
         self.display.text = [self.display.text stringByAppendingString:digit];
-    } else {
+    } else {  //also new digit, but with last typed digits covered
         self.display.text = digit;
-        self.userIsInTheMiddleOfEnteringANumber = YES;
+        //self.userIsInTheMiddleOfEnteringANumber = YES;
+       self.displayLastPressed=1;
     }
 }
 
 - (IBAction)floatPointPressed:(id)sender {
-    if (_IsAfterFloatPoint){
+    //if (_IsAfterFloatPoint){
+    if (self.displayLastPressed==20 ||self.displayLastPressed==21) {
         //do nothing
         NSLog(@"Can't type float point twice!");
     }
-    else if(self.userIsInTheMiddleOfEnteringANumber){
-        _IsAfterFloatPoint = YES;
+    //else if(self.userIsInTheMiddleOfEnteringANumber){
+    else if(self.displayLastPressed==1){
+        self.displayLastPressed=20; //_IsAfterFloatPoint = YES;
         self.display.text = [self.display.text stringByAppendingString:@"."];
     }
-    else{
-        _IsAfterFloatPoint = YES;
+    else if(self.displayLastPressed==0){
+        self.displayLastPressed=20; 
         self.display.text = @"0.";
-        self.userIsInTheMiddleOfEnteringANumber = YES;
+        //_IsAfterFloatPoint = YES;
+        //self.userIsInTheMiddleOfEnteringANumber = YES;
     }
 }
 
 - (IBAction)enterPressed {
     [self.brain pushOperand:[self.display.text doubleValue]];
-    self.userIsInTheMiddleOfEnteringANumber = NO;
-    self.IsAfterFloatPoint = NO;
+    //self.userIsInTheMiddleOfEnteringANumber = NO;
+    //self.IsAfterFloatPoint = NO;
+    self.displayLastPressed=0;
+    self.display2LastPressed=0;
 }
 
 - (IBAction)operationPressed:(id)sender {
-    if (self.userIsInTheMiddleOfEnteringANumber){
-        [self enterPressed];
-         self.userIsInTheMiddleOfEnteringANumber = NO;
+    if (self.displayLastPressed==20) {
+        NSLog(@"Can't use in uncompleted digit!");
+        return;
     }
-    NSString *operation = [sender currentTitle];
-    double result = [self.brain performOperation:operation];
-    self.display.text = [NSString stringWithFormat:@"%g", result];
+    else if (self.displayLastPressed==0) {
+        NSLog(@"Can't push 0 so far");
+        //?? need to be improved
+        return;
+    }else { // inTheMiddleOfEnteringNumber
+        [self enterPressed];
+        self.displayLastPressed=0;
+        NSString *operation = [sender currentTitle];
+        double result = [self.brain performOperation:operation];
+        self.display.text = [NSString stringWithFormat:@"%g", result];
+    }
 }
 
 - (IBAction)clearEverything {
     self.display2.text = @"0";
     self.display.text = @"0";
     [self.brain clearStack];
-    _display2LastPressed=0;
-    _IsAfterFloatPoint=NO;
+    self.display2LastPressed=0;
+    //_IsAfterFloatPoint=NO;
+    self.displayLastPressed=0;
 }
+
 
 - (IBAction)displayAllOperations:(id)sender {
     NSString *digit;
@@ -147,6 +164,7 @@
     }
 }
 
+ 
 /*
  Bug: can't make space, don't know which event first?
  how to use userIsInTheMiddleOfEnteringANumber?

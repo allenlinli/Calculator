809c86fd50cd2baaae18accdfa1d5ca710fe358b
allenlinli <allenlinli@gmail.com>
2012-10-11 22:54:54 +0800

    temp

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -112,6 +112,7 @@
 
 + (NSString *)descriptionOfProgram:(id)program
 {
+
     return @"Implement this in Homework #2";
 }
 
@@ -122,6 +123,7 @@
 + (NSSet *)variablesUsedInProgram:(const id) program
 {
     NSSet * variables = nil;
+    NSSet * xab = [NSSet setWithArray:@[@"x",@"a",@"b"]];
     NSUInteger programLengh = [program count];
     if (programLengh==0) {
         return nil;
@@ -129,16 +131,66 @@
     else{
         for (NSUInteger i=0; i<programLengh; i++) {
             NSString *variable = program[i];
-            if ([variable isEqualToString:@"x"] ||
+            if ([xab containsObject:variable]){
+               [variables setByAddingObject:variable];
+               /*([variable isEqualToString:@"x"] ||
                 [variable isEqualToString:@"a"] ||
-                [variable isEqualToString:@"x"]) {
-                [variables setByAddingObject:variable];
+                [variable isEqualToString:@"b"]) {
+                [variables setByAddingObject:variable];*/
             }
         }
         return variables;
     }
 }
 
+-(NSNumber *)numberOfOperandsINAOperation :(NSString*) operation
+{
+    NSSet *oneOperandOperaion = [NSSet setWithArray:@[@"sqrt",@"π"]];
+}
+
+/*
++ (NSString*) descriptionOfTopOfStack::(NSMutableArray *) stack
+{
+    NSString* description = nil;
+    //debugger//
+    //    NSLog(@"stack content");
+    //    for (int i=0; i<[stack count]; i++) {
+    //        NSLog(@"%@",[stack objectAtIndex:i]);
+    //        NSLog(@"%@",[[stack objectAtIndex:i] class]);
+    //    }
+    
+    id topOfStack = [stack lastObject];
+    if (topOfStack) [stack removeLastObject];
+    
+    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+        result = [topOfStack doubleValue];
+    }
+    else if([topOfStack isKindOfClass:[NSString class]]){
+        NSString *operation = topOfStack;
+        if ([operation isEqualToString:@"+"]) {
+            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
+        } else if([@"*" isEqualToString:operation]){
+            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
+        } else if ([operation isEqualToString:@"-"]){
+            double subtrashend = [self popOperandOffStack:stack];
+            result = [self popOperandOffStack:stack] - subtrashend;
+        } else if ([operation isEqualToString:@"/"]){
+            double divisor = [self popOperandOffStack:stack];
+            if (divisor) result = [self popOperandOffStack:stack]/divisor;
+        } else if([@"sin" isEqualToString:operation]){
+            result = sin([self popOperandOffStack:stack]);
+        } else if([@"cos" isEqualToString:operation]){
+            result = cos([self popOperandOffStack:stack]);
+        } else if([@"sqrt" isEqualToString:operation]){
+            result = sqrt([self popOperandOffStack:stack]);
+        } else if([@"π" isEqualToString:operation]){
+            result = [self popOperandOffStack:stack];  //?? not complete
+        }
+    }
+    
+    return result;
+}
+*/
 
 + (double) popOperandOffStack:(NSMutableArray *) stack
 {

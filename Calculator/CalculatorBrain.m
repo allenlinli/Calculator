87293911f65b0f4ff3197ae96a7cdfa62d173cf0
allenlinli <allenlinli@gmail.com>
2012-10-12 10:56:45 +0800

    temp

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -54,14 +54,14 @@
 
 - (void)pushOperand:(NSString*)operand
 {
+    if ([[self class] isOperation:operand]) {
+        [self.programStack addObject:operand];
+    }
     if ([operand doubleValue]!=0.0) {
         [self.programStack addObject:@([operand doubleValue])];
     }
     else if([operand isEqualToString:@"0"]){
         [self.programStack addObject:@([operand doubleValue])];
-    }
-    else{
-        [self.programStack addObject:operand];
     }
 }
 
@@ -174,49 +174,25 @@
     }
 }
 
-/*
-+ (NSString*) descriptionOfTopOfStack::(NSMutableArray *) stack
+
++ (NSString*) descriptionOfTopOfStack:(NSMutableArray *) stack
 {
-    NSString* description = nil;
-    //debugger//
-    //    NSLog(@"stack content");
-    //    for (int i=0; i<[stack count]; i++) {
-    //        NSLog(@"%@",[stack objectAtIndex:i]);
-    //        NSLog(@"%@",[[stack objectAtIndex:i] class]);
-    //    }
+    NSString* description = @"";
     
     id topOfStack = [stack lastObject];
     if (topOfStack) [stack removeLastObject];
     
     if ([topOfStack isKindOfClass:[NSNumber class]]) {
-        result = [topOfStack doubleValue];
+        [description stringByAppendingFormat:[NSString stringWithFormat:@"%@",(NNS)topOfStack]];
     }
-    else if([topOfStack isKindOfClass:[NSString class]]){
+    else{
         NSString *operation = topOfStack;
-        if ([operation isEqualToString:@"+"]) {
-            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
-        } else if([@"*" isEqualToString:operation]){
-            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
-        } else if ([operation isEqualToString:@"-"]){
-            double subtrashend = [self popOperandOffStack:stack];
-            result = [self popOperandOffStack:stack] - subtrashend;
-        } else if ([operation isEqualToString:@"/"]){
-            double divisor = [self popOperandOffStack:stack];
-            if (divisor) result = [self popOperandOffStack:stack]/divisor;
-        } else if([@"sin" isEqualToString:operation]){
-            result = sin([self popOperandOffStack:stack]);
-        } else if([@"cos" isEqualToString:operation]){
-            result = cos([self popOperandOffStack:stack]);
-        } else if([@"sqrt" isEqualToString:operation]){
-            result = sqrt([self popOperandOffStack:stack]);
-        } else if([@"π" isEqualToString:operation]){
-            result = [self popOperandOffStack:stack];  //?? not complete
-        }
+        
     }
     
-    return result;
+    return description;
 }
-*/
+
 
 + (double) popOperandOffStack:(NSMutableArray *) stack
 {
@@ -264,28 +240,19 @@
 - (int)differentiateOperation: (NSString*)operation
 //return value: 0 for no-operand operation, 1 for single-operand operation, 2 for two-operand operation, and -1 for operand, -2 for dot
 {
+    NSSet *oneOperandsOperaion = [NSSet setWithArray:@[@"sqrt",@"cos",@"sin"]];
+    NSSet *twoOperandsOperaion = [NSSet setWithArray:@[@"+",@"-",@"*",@"/"]];
+    NSSet *zeroOperandsOperaion = [NSSet setWithArray:@[@"π",@"x",@"a",@"b"]];
+    
     if([operation doubleValue])  return -1;
-    else if([operation isEqualToString:@"0"]) return -1;
+    //else if([operation isEqualToString:@"0"]) return -1;
     
     else if([operation isEqualToString:@"."])   return -2;
-    
-    else if([operation isEqualToString:@"sqrt"] ||
-            [operation isEqualToString:@"sin"]  ||
-            [operation isEqualToString:@"cos"])  return 1;
-    
-    else if([operation isEqualToString:@"π"] ||
-            [operation isEqualToString:@"x"] ||
-            [operation isEqualToString:@"a"] ||
-            [operation isEqualToString:@"b"])  return 0;
-    
-    else if([operation isEqualToString:@"+"] ||
-            [operation isEqualToString:@"-"] ||
-            [operation isEqualToString:@"*"] ||
-            [operation isEqualToString:@"/"])  return 2;
-    else{
-        NSLog(@"bug!!");
-        return -99;
-    }
+    else if([oneOperandsOperaion containsObject:operation]) return 1;
+    else if ([twoOperandsOperaion containsObject:operation]) return 2;
+    else if([zeroOperandsOperaion containsObject:operation]) return 0;
+    else if([operation isEqualToString:@"Enter"]) return 3;
+    else    {NSLog(@"bug!!"); return -99;}
 }
 
 
